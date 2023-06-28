901a0acedd58251f801221910354752f451d4511
kienbeers <kienht0709@gmail.com>
2023-06-28 22:08:18 +0700

    Thymeleaf

---
+++ thymeleaf/src/main/java/com/ses/thymeleaf/form/PersonForm.java
@@ -0,0 +1,22 @@
+package com.ses.thymeleaf.form;
+
+public class PersonForm {
+    private String lastName;
+    private String firstName;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+}
